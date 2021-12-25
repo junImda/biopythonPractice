11cd7c70f019c1323da711b98a62227d09fd33b8
Junsang Yoon <junyoon@berkeley.edu>
2021-12-24 23:21:24 -0800

    translate ABO gene

--- test.py
+++ test.py
@@ -1,5 +1,27 @@
 import csv
 import numpy as np
+import pandas as pd
 import Bio
 
-print(Bio.__version__)
+from Bio.Seq import Seq
+
+df = pd.read_csv("ABOgene.csv")
+
+colDNA = df["Reference"]
+
+sequenceInStr = ""
+colLength = len(colDNA.index)
+
+for i in np.arange(0, colLength, 1):
+    if len(colDNA.array[i]) == 0:
+        sequenceInStr = sequenceInStr + colDNA.array[i]
+    else:
+        sequenceInStr = sequenceInStr + colDNA.array[i][0]
+
+dnaseq = Seq(sequenceInStr)
+mRNA = dnaseq.reverse_complement().transcribe()
+proteinSeq = mRNA.translate()
+
+print(proteinSeq)
+
+print(len(proteinSeq) * 3)
